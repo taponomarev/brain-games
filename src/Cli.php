2be1feb0a7da35a848ce7847decf73406b3e017e
Alexey Ponomarev <taponomarev@gmail.com>
2020-12-19 11:45:01 +0000

    Step three completed

---
+++ src/Cli.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Brain\Games\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+line($msg = 'Welcome to the Brain Games!');
+$name = prompt('May I have your name?');
+line("Hello, %s!", $name);
