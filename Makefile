f62c41da89f364f1d185a69a3eb1653050700cc5
Alexey Ponomarev <taponomarev@gmail.com>
2020-12-19 16:51:43 +0000

    Added github action linter

--- Makefile
+++ Makefile
@@ -2,6 +2,9 @@ install:
 	composer install
 validate:
 	composer validate
-		
+
 brain-games:
 	./bin/brain-games
+
+lint:
+	composer run-script phpcs -- --standard=PSR12 src bin	
--- src/Cli.php
+++ src/Cli.php
@@ -4,7 +4,16 @@ namespace Brain\Games\Cli;
 
 use function cli\line;
 use function cli\prompt;
+use function cli\err;
 
-line($msg = 'Welcome to the Brain Games!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+function run()
+{
+    line('Welcome to the Brain Games!');
+
+    try {
+        $name = prompt('May I have your name?');
+        line("Hello, %s!", $name);
+    } catch (\Exception $e) {
+        err($e->getMessage());
+    }
+}
