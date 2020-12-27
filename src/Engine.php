b617df1d0b3e5b3d0714d6186b9204df39b52203
Alexey Ponomarev <taponomarev@gmail.com>
2020-12-27 14:30:02 +0000

    Fixed spaces

--- src/Engine.php
+++ src/Engine.php
@@ -9,35 +9,35 @@ use function cli\err;
 function run(callable $game)
 {
 
-	line('Welcome to the Brain Games!');
-	$username = prompt('May I have your name?');
-	line("Hello, %s!", $username);
-	tickGame($game, $username);
+    line('Welcome to the Brain Games!');
+    $username = prompt('May I have your name?');
+    line("Hello, %s!", $username);
+    tickGame($game, $username);
 }
 
 function tickGame(callable $game, string $username, int $currentLevel = 3)
 {
-	if (!$currentLevel) {
-		line("Congratulations, {$username}");
-		return;
-	}
+    if (!$currentLevel) {
+        line("Congratulations, {$username}");
+        return;
+    }
 
-	$maxLevel = 3;
-	['expression' => $expression, 'correctAnswer' => $correctAnswer, 'welcomeMsg' => $welcomeMsg] = $game();
+    $maxLevel = 3;
+    ['expression' => $expression, 'correctAnswer' => $correctAnswer, 'welcomeMsg' => $welcomeMsg] = $game();
 
-	if ($currentLevel === $maxLevel) {
-		line($welcomeMsg);
-	}
+    if ($currentLevel === $maxLevel) {
+        line($welcomeMsg);
+    }
 
-	$userAnswer = prompt("Question: {$expression}");
-	$errorMsg = "'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'";
+    $userAnswer = prompt("Question: {$expression}");
+    $errorMsg = "'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'";
 
-	if ($userAnswer == $correctAnswer) {
-		line('Correct!');
-		tickGame($game, $username, $currentLevel -= 1);
-	} else {
-		err($errorMsg);
-		err("Let's try again, {$username}!");
-		return;
-	}
+    if ($userAnswer == $correctAnswer) {
+        line('Correct!');
+        tickGame($game, $username, $currentLevel -= 1);
+    } else {
+        err($errorMsg);
+        err("Let's try again, {$username}!");
+        return;
+    }
 }
